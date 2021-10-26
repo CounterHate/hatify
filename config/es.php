00af74b8c730885aeba95ca2ccfe1a7b5bc1493a
Paweł Korpal <pawel.korpal@adm.uw.edu.pl>
2021-10-26 12:55:00 +0200

    created es config file

---
+++ config/es.php
@@ -0,0 +1,1 @@
+<?php return [ 'ES' => 'https://es.dc9.dev:9200', 'INDEX' => 'tweets', 'ES_USER' => 'user', 'ES_PASS' => 'pass'];
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layout')
 @section('content')
-    <show-tweets url={{ ENV('ES') }} index={{ ENV('INDEX') }} :search="false" es_user={{ ENV('ES_USER') }} es_pass={{ ENV('ES_PASS') }} />
+    <show-tweets url={{ config('es.ES') }} index={{ config('es.INDEX') }} :search="false" es_user={{ config('es.ES_USER') }}
+        es_pass={{ config('es.ES_PASS') }} />
 
 @endsection
