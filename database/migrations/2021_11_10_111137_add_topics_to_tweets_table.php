582dd2eb63532cc6701c320b7ae09eafd71b31e4
Paweł Korpal <pawel.korpal@adm.uw.edu.pl>
2021-11-10 12:13:10 +0100

    added add_topics_to_tweets migration

--- database/migrations/2021_11_10_111137_add_topics_to_tweets_table.php
+++ database/migrations/2021_11_10_111137_add_topics_to_tweets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTweetsTable extends Migration
+class AddTopicsToTweetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,8 @@ class CreateTweetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tweets', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('tweet_id');
-            $table->string('author');
-            $table->text('content');
-            $table->string('date');
+        Schema::table('tweets', function (Blueprint $table) {
+            $table->json('topics');
         });
     }
 
@@ -30,6 +25,8 @@ class CreateTweetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tweets');
+        Schema::table('tweets', function (Blueprint $table) {
+            //
+        });
     }
 }
