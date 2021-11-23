0bff351f30e45d0e9e638f5977a91e0f1a1230a5
Paweł Korpal <pawel.korpal@adm.uw.edu.pl>
2021-11-23 11:52:43 +0100

    changed anotation view

--- public/js/app.js
+++ public/js/app.js
@@ -21138,9 +21138,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       auth: {
         username: "dc9",
         password: "hohC2wix"
-      },
-      totalAnotationCount: 0,
-      userAnotationCount: 0
+      }
     };
   },
   mounted: function mounted() {
@@ -21157,10 +21155,6 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
               });
 
             case 2:
-              _context.next = 4;
-              return _this.getAnotationCount();
-
-            case 4:
             case "end":
               return _context.stop();
           }
@@ -21169,7 +21163,8 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
     }))();
   },
   methods: {
-    getAnotationCount: function getAnotationCount() {
+    getRandomTweets: _es_js__WEBPACK_IMPORTED_MODULE_2__.getRandomTweets,
+    handleSkipTweet: function handleSkipTweet() {
       var _this2 = this;
 
       return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee2() {
@@ -21178,16 +21173,8 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
             switch (_context2.prev = _context2.next) {
               case 0:
                 _context2.next = 2;
-                return axios.get("/api/tweets").then(function (response) {
-                  var data = response.data.data;
-                  _this2.totalAnotationCount = data.length;
-                  var tmpUserAnotationCount = 0;
-                  data.forEach(function (el) {
-                    if (JSON.parse(el.topics).user == _this2.user) tmpUserAnotationCount++;
-                  });
-                  _this2.userAnotationCount = tmpUserAnotationCount;
-                })["catch"](function (error) {
-                  return console.log(error);
+                return (0,_es_js__WEBPACK_IMPORTED_MODULE_2__.getRandomTweets)(1, _this2.url, _this2.auth).then(function (result) {
+                  return _this2.tweet = result[0];
                 });
 
               case 2:
@@ -21198,36 +21185,14 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
         }, _callee2);
       }))();
     },
-    getRandomTweets: _es_js__WEBPACK_IMPORTED_MODULE_2__.getRandomTweets,
-    handleSkipTweet: function handleSkipTweet() {
+    handleProcessTweet: function handleProcessTweet(data) {
       var _this3 = this;
 
       return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee3() {
+        var tweet;
         return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee3$(_context3) {
           while (1) {
             switch (_context3.prev = _context3.next) {
-              case 0:
-                _context3.next = 2;
-                return (0,_es_js__WEBPACK_IMPORTED_MODULE_2__.getRandomTweets)(1, _this3.url, _this3.auth).then(function (result) {
-                  return _this3.tweet = result[0];
-                });
-
-              case 2:
-              case "end":
-                return _context3.stop();
-            }
-          }
-        }, _callee3);
-      }))();
-    },
-    handleProcessTweet: function handleProcessTweet(data) {
-      var _this4 = this;
-
-      return _asyncToGenerator( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().mark(function _callee4() {
-        var tweet;
-        return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default().wrap(function _callee4$(_context4) {
-          while (1) {
-            switch (_context4.prev = _context4.next) {
               case 0:
                 if (data.is_hate_speech) {
                   tweet = {
@@ -21236,9 +21201,8 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
                     content: data.tweet.content,
                     date: (data.tweet.posted_utime * 1000).toString(),
                     topics: JSON.stringify({
-                      user: _this4.user,
-                      topics: data.topics,
-                      input_topic: data.input_topic
+                      user: _this3.user,
+                      topics: data.topics
                     })
                   };
                   axios.post("api/tweets", tweet).then(function (response) {
@@ -21248,21 +21212,17 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
                   });
                 }
 
-                _context4.next = 3;
-                return (0,_es_js__WEBPACK_IMPORTED_MODULE_2__.getRandomTweets)(1, _this4.url, _this4.auth).then(function (result) {
-                  return _this4.tweet = result[0];
+                _context3.next = 3;
+                return (0,_es_js__WEBPACK_IMPORTED_MODULE_2__.getRandomTweets)(1, _this3.url, _this3.auth).then(function (result) {
+                  return _this3.tweet = result[0];
                 });
 
               case 3:
-                _context4.next = 5;
-                return _this4.getAnotationCount();
-
-              case 5:
               case "end":
-                return _context4.stop();
+                return _context3.stop();
             }
           }
-        }, _callee4);
+        }, _callee3);
       }))();
     }
   }
@@ -21656,19 +21616,19 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
 
+var _hoisted_1 = {
+  "class": "row"
+};
 
-var _hoisted_1 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("br", null, null, -1
-/* HOISTED */
-);
+var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"col\"><h2>Czym jest mowa nienawiści</h2> Mowa nienawiści to wszelkie formy nienawistnych wypowiedzi kierowanych do ludzi ze względu na ich daną (przyrodzoną, niezbywalną) cechę ( kolor skóry, język, religię lub wyznanie, pochodzenie etniczne czy narodowe, wiek, niepełnosprawność, płeć, społeczno-kulturowa tożsamość płciowa i orientacja psychoseksualna ). <br><br><h2> Jeśli masz problem ze stwierdzenie, czy dana wypowiedź to mowa nienawiści, to zastanów się czy: </h2><ul><li> odnosi się do grupy/osoby w związku jakąś jej cechą, - zawiera obraźliwe określenia grup/osób, </li><li>nawołuje do agresji, podżega do nienawiści,</li><li>zawiera groźby,</li><li>zawiera wyrazy nienawiści,</li><li>zawiera wyrazy obrzydzenia,</li><li> zawiera wyrazy obrzydzenia,pogardy, dehumanizacji, znieważenia, </li><li> zawiera negatywne stereotypy, tezy oczerniające lub szkalujące, </li><li> usprawiedliwia agresję, dyskryminację, gorsze traktowanie jakieś osoby/grupy, - wyraża agresywny nacjonalizm </li></ul></div>", 1);
 
+var _hoisted_3 = {
+  "class": "col"
+};
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   var _component_tweet = (0,vue__WEBPACK_IMPORTED_MODULE_0__.resolveComponent)("tweet");
 
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" Liczba wszystkich anotowanych tweetów: " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(this.totalAnotationCount) + " ", 1
-  /* TEXT */
-  ), _hoisted_1, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)("Liczba anotowanych tweetów uzytkownika: " + (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)(this.userAnotationCount) + " ", 1
-  /* TEXT */
-  ), this.tweet != null ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(_component_tweet, {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, [_hoisted_2, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_3, [this.tweet != null ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(_component_tweet, {
     key: 0,
     random: true,
     data: this.tweet,
@@ -21676,9 +21636,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
     onSkip_tweet: $options.handleSkipTweet
   }, null, 8
   /* PROPS */
-  , ["data", "onProcess_tweet", "onSkip_tweet"])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)], 64
-  /* STABLE_FRAGMENT */
-  );
+  , ["data", "onProcess_tweet", "onSkip_tweet"])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)])]);
 }
 
 /***/ }),
@@ -58756,13 +58714,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _RandomTweet_vue_vue_type_template_id_15538753__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./RandomTweet.vue?vue&type=template&id=15538753 */ "./resources/js/components/RandomTweet.vue?vue&type=template&id=15538753");
 /* harmony import */ var _RandomTweet_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./RandomTweet.vue?vue&type=script&lang=js */ "./resources/js/components/RandomTweet.vue?vue&type=script&lang=js");
-/* harmony import */ var _Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var _Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
 
 ;
-const __exports__ = /*#__PURE__*/(0,_Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_RandomTweet_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_RandomTweet_vue_vue_type_template_id_15538753__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/RandomTweet.vue"]])
+const __exports__ = /*#__PURE__*/(0,_Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_RandomTweet_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_RandomTweet_vue_vue_type_template_id_15538753__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/RandomTweet.vue"]])
 /* hot reload */
 if (false) {}
 
@@ -58785,7 +58743,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _ShowTweets_vue_vue_type_template_id_2ed732aa__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ShowTweets.vue?vue&type=template&id=2ed732aa */ "./resources/js/components/ShowTweets.vue?vue&type=template&id=2ed732aa");
 /* harmony import */ var _ShowTweets_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ShowTweets.vue?vue&type=script&lang=js */ "./resources/js/components/ShowTweets.vue?vue&type=script&lang=js");
 /* harmony import */ var _ShowTweets_vue_vue_type_style_index_0_id_2ed732aa_lang_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./ShowTweets.vue?vue&type=style&index=0&id=2ed732aa&lang=css */ "./resources/js/components/ShowTweets.vue?vue&type=style&index=0&id=2ed732aa&lang=css");
-/* harmony import */ var _Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var _Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
@@ -58793,7 +58751,7 @@ __webpack_require__.r(__webpack_exports__);
 ;
 
 
-const __exports__ = /*#__PURE__*/(0,_Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__["default"])(_ShowTweets_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_ShowTweets_vue_vue_type_template_id_2ed732aa__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/ShowTweets.vue"]])
+const __exports__ = /*#__PURE__*/(0,_Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__["default"])(_ShowTweets_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_ShowTweets_vue_vue_type_template_id_2ed732aa__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/ShowTweets.vue"]])
 /* hot reload */
 if (false) {}
 
@@ -58815,13 +58773,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _TopicSelect_vue_vue_type_template_id_8a1ee720__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./TopicSelect.vue?vue&type=template&id=8a1ee720 */ "./resources/js/components/TopicSelect.vue?vue&type=template&id=8a1ee720");
 /* harmony import */ var _TopicSelect_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./TopicSelect.vue?vue&type=script&lang=js */ "./resources/js/components/TopicSelect.vue?vue&type=script&lang=js");
-/* harmony import */ var _Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var _Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
 
 ;
-const __exports__ = /*#__PURE__*/(0,_Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_TopicSelect_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_TopicSelect_vue_vue_type_template_id_8a1ee720__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/TopicSelect.vue"]])
+const __exports__ = /*#__PURE__*/(0,_Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_TopicSelect_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_TopicSelect_vue_vue_type_template_id_8a1ee720__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/TopicSelect.vue"]])
 /* hot reload */
 if (false) {}
 
@@ -58843,13 +58801,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _Tweet_vue_vue_type_template_id_4683c2d4__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Tweet.vue?vue&type=template&id=4683c2d4 */ "./resources/js/components/Tweet.vue?vue&type=template&id=4683c2d4");
 /* harmony import */ var _Tweet_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Tweet.vue?vue&type=script&lang=js */ "./resources/js/components/Tweet.vue?vue&type=script&lang=js");
-/* harmony import */ var _Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var _Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
 
 ;
-const __exports__ = /*#__PURE__*/(0,_Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_Tweet_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_Tweet_vue_vue_type_template_id_4683c2d4__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/Tweet.vue"]])
+const __exports__ = /*#__PURE__*/(0,_Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_Tweet_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_Tweet_vue_vue_type_template_id_4683c2d4__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/Tweet.vue"]])
 /* hot reload */
 if (false) {}
 
@@ -58871,13 +58829,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var _TweetAnotationButtons_vue_vue_type_template_id_36d5fe54__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./TweetAnotationButtons.vue?vue&type=template&id=36d5fe54 */ "./resources/js/components/TweetAnotationButtons.vue?vue&type=template&id=36d5fe54");
 /* harmony import */ var _TweetAnotationButtons_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./TweetAnotationButtons.vue?vue&type=script&lang=js */ "./resources/js/components/TweetAnotationButtons.vue?vue&type=script&lang=js");
-/* harmony import */ var _Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var _Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
 
 ;
-const __exports__ = /*#__PURE__*/(0,_Users_pawelkorpal_Code_laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_TweetAnotationButtons_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_TweetAnotationButtons_vue_vue_type_template_id_36d5fe54__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/TweetAnotationButtons.vue"]])
+const __exports__ = /*#__PURE__*/(0,_Users_pkorpal_Code_Laravel_Sites_hatify_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_2__["default"])(_TweetAnotationButtons_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_TweetAnotationButtons_vue_vue_type_template_id_36d5fe54__WEBPACK_IMPORTED_MODULE_0__.render],['__file',"resources/js/components/TweetAnotationButtons.vue"]])
 /* hot reload */
 if (false) {}
 
--- resources/js/components/RandomTweet.vue
+++ resources/js/components/RandomTweet.vue
@@ -1,90 +1,109 @@
 <template>
-  Liczba wszystkich anotowanych tweetów: {{ this.totalAnotationCount }}
-  <br />Liczba anotowanych tweetów uzytkownika: {{ this.userAnotationCount }}
-  <tweet
-    v-if="this.tweet != null"
-    :random="true"
-    :data="this.tweet"
-    @process_tweet="handleProcessTweet"
-    @skip_tweet="handleSkipTweet"
-  ></tweet>
+    <div class="row">
+        <div class="col">
+            <h2>Czym jest mowa nienawiści</h2>
+            Mowa nienawiści to wszelkie formy nienawistnych wypowiedzi
+            kierowanych do ludzi ze względu na ich daną (przyrodzoną,
+            niezbywalną) cechę ( kolor skóry, język, religię lub wyznanie,
+            pochodzenie etniczne czy narodowe, wiek, niepełnosprawność, płeć,
+            społeczno-kulturowa tożsamość płciowa i orientacja psychoseksualna
+            ).
+            <br />
+            <br />
+            <h2>
+                Jeśli masz problem ze stwierdzenie, czy dana wypowiedź to mowa
+                nienawiści, to zastanów się czy:
+            </h2>
+            <ul>
+                <li>
+                    odnosi się do grupy/osoby w związku jakąś jej cechą, -
+                    zawiera obraźliwe określenia grup/osób,
+                </li>
+                <li>nawołuje do agresji, podżega do nienawiści,</li>
+                <li>zawiera groźby,</li>
+                <li>zawiera wyrazy nienawiści,</li>
+                <li>zawiera wyrazy obrzydzenia,</li>
+                <li>
+                    zawiera wyrazy obrzydzenia,pogardy, dehumanizacji,
+                    znieważenia,
+                </li>
+                <li>
+                    zawiera negatywne stereotypy, tezy oczerniające lub
+                    szkalujące,
+                </li>
+                <li>
+                    usprawiedliwia agresję, dyskryminację, gorsze traktowanie
+                    jakieś osoby/grupy, - wyraża agresywny nacjonalizm
+                </li>
+            </ul>
+        </div>
+        <div class="col">
+            <tweet
+                v-if="this.tweet != null"
+                :random="true"
+                :data="this.tweet"
+                @process_tweet="handleProcessTweet"
+                @skip_tweet="handleSkipTweet"
+            ></tweet>
+        </div>
+    </div>
 </template>
 
 <script>
 import Tweet from "./Tweet.vue";
 import { getRandomTweets } from "../es.js";
 export default {
-  components: { Tweet },
-  props: {
-    user: Number,
-  },
-  data() {
-    return {
-      is_loading: false,
-      tweet: null,
-      url: process.env.MIX_ES,
-      index: process.env.MIX_INDEX,
-      auth: {
-        username: process.env.MIX_ES_USER,
-        password: process.env.MIX_ES_PASS,
-      },
-      totalAnotationCount: 0,
-      userAnotationCount: 0,
-    };
-  },
-  async mounted() {
-    await getRandomTweets(1, this.url + "/" + this.index, this.auth).then(
-      (result) => (this.tweet = result[0])
-    );
-    await this.getAnotationCount();
-  },
-  methods: {
-    async getAnotationCount() {
-      await axios
-        .get("/api/tweets")
-        .then((response) => {
-          var data = response.data.data;
-          this.totalAnotationCount = data.length;
-          var tmpUserAnotationCount = 0;
-          data.forEach((el) => {
-            if (JSON.parse(el.topics).user == this.user)
-              tmpUserAnotationCount++;
-          });
-          this.userAnotationCount = tmpUserAnotationCount;
-        })
-        .catch((error) => console.log(error));
+    components: { Tweet },
+    props: {
+        user: Number,
     },
-    getRandomTweets,
-    async handleSkipTweet() {
-      await getRandomTweets(1, this.url, this.auth).then(
-        (result) => (this.tweet = result[0])
-      );
-    },
-    async handleProcessTweet(data) {
-      if (data.is_hate_speech) {
-        var tweet = {
-          tweet_id: data.tweet.tweet_id,
-          author: data.tweet.author_username,
-          content: data.tweet.content,
-          date: (data.tweet.posted_utime * 1000).toString(),
-          topics: JSON.stringify({
-            user: this.user,
-            topics: data.topics,
-            input_topic: data.input_topic,
-          }),
+    data() {
+        return {
+            is_loading: false,
+            tweet: null,
+            url: process.env.MIX_ES,
+            index: process.env.MIX_INDEX,
+            auth: {
+                username: process.env.MIX_ES_USER,
+                password: process.env.MIX_ES_PASS,
+            },
         };
-        axios
-          .post("api/tweets", tweet)
-          .then((response) => console.log(response.data))
-          .catch((error) => console.error(error));
-      }
-
-      await getRandomTweets(1, this.url, this.auth).then(
-        (result) => (this.tweet = result[0])
-      );
-      await this.getAnotationCount();
     },
-  },
+    async mounted() {
+        await getRandomTweets(1, this.url + "/" + this.index, this.auth).then(
+            (result) => (this.tweet = result[0])
+        );
+    },
+    methods: {
+        getRandomTweets,
+        async handleSkipTweet() {
+            await getRandomTweets(1, this.url, this.auth).then(
+                (result) => (this.tweet = result[0])
+            );
+        },
+        async handleProcessTweet(data) {
+            if (data.is_hate_speech) {
+                var tweet = {
+                    tweet_id: data.tweet.tweet_id,
+                    author: data.tweet.author_username,
+                    content: data.tweet.content,
+                    date: (data.tweet.posted_utime * 1000).toString(),
+                    topics: JSON.stringify({
+                        user: this.user,
+                        topics: data.topics,
+                    }),
+                };
+                axios
+                    .post("api/tweets", tweet)
+                    .then((response) => console.log(response.data))
+                    .catch((error) => console.error(error));
+            }
+
+            await getRandomTweets(1, this.url, this.auth).then(
+                (result) => (this.tweet = result[0])
+            );
+        },
+    },
 };
 </script>
 
