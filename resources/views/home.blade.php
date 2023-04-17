bb29cd92112644dc539c806a64321b49823e6de2
Pawel Korpal <pawelkorpal@macbook-air-pawel.home>
2023-04-17 17:11:47 +0200

    updated home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,36 @@
 @section('content')
     <div class="card card-frame" style="margin-bottom: 16px">
         <div class="card-body">
+            <h3>Powitanie</h3>
+            Wzdrygasz się na widok nienawiści rozlewającej się na Twitterze? Chcesz włączyć się w przeciwdziałanie jej?
+            Działaj z nami!<br><br>
+
+            CounterHate to system do monitorowania internetowej mowy nienawiści. Jeśli nie wystarcza Ci zgłaszanie
+            nienawistnych komentarzy – chcesz wiedzieć o tym więcej i działać jeszcze więcej przeciwko nim, to może być
+            narzędzie dla Ciebie. <br><br>
+
+            Chcesz wiedzieć:
+            <ul>
+                <li> które toksyczne narracje są najpowszechniejsze?
+                </li>
+                <li> jak zmieniają się w czasie?
+                </li>
+                <li> które konta szerzą najwięcej (i jakich) nienawistnych treści?
+                </li>
+            </ul>
+
+
+
+            Dzięki zastosowaniu uczenia maszynowego, CounterHate odfiltrowuje z Twittera nienawistne treści – nawet te,
+            które zostały już skasowane. Pozwala je monitorować i analizować, ogólnie i z podziałem na bardziej szczegółowe
+            narracje i kategorie. <br><br>
+
+            Zacznij monitorować!
+        </div>
+    </div>
+
+    {{-- <div class="card card-frame" style="margin-bottom: 16px">
+        <div class="card-body">
             <h5>Witaj na stronie systemu do monitorowania mowy nienawiści. </h5><br>
 
             Chcesz dowiedzieć się, które toksyczne narracje są najpowszechniejsze? <br><br>
@@ -12,109 +42,86 @@
 
             CounterHate odfiltrowuje z Twittera nienawistne treści i pozwala je monitorować i analizować.
         </div>
+    </div> --}}
+
+    <div class="card card-frame" style="margin-bottom: 16px">
+        <div class="card-body">
+            <h3>Mozliwości i ograniczenia systemu </h3>
+            <h5>Wyszukiwanie treści</h5>
+
+            Pozwala na wyszukiwanie w bazie wpisów mowy nienawiści pod względem jej rodzaju (kategorii), zawartości (treści,
+            konkretnych słów), kont czy zakresu czasu.<br><br>
+
+            <h5>Wykresy i rankingi</h5>
+
+            Przeszukiwane treści można wizualizować na wykresach i rankingach. Ta opcja pozwala na prześledzenie zmian
+            nasilenia mowy nienawiści w czasie, tworzenie rankingów nienawistnych kont lub kluczowych nienawistnych słów.
+            Wykresy można pobierać jako pliki graficzne.<br><br>
+
+            <h5>Pobieranie danych</h5>
+
+            Wyszukane treści można pobrać w formie arkusza kalkulacyjnego.<br><br>
+
+            <h5>Sprawdzenie kontekstu wypowiedzi</h5>
+
+            Każdy wpis można zobaczyć w oryginalnym kontekście na Twitterze i sprawdzić, jak prezentuje się ona w szerszym
+            kontekście – czy jest odpowiedzią na coś, czy jest elementem dyskusji, itp. Ta opcja ułatwia badanie dynamik
+            rozpowszechniania się nienawistnych treści.<br><br>
+
+            <h5>Anotowanie treści</h5>
+
+            Pozwala użytkownikom na ocenę zebranych przez system treści pod kątem tego, czy należy uznać je za mowę
+            nienawiści, czy nie – a jeśli tak, to jakiej grupy dotyczy nienawiść. Jest to więc „cegiełka”, którą użytkownik
+            może dołożyć do rozwoju systemu – dzięki temu algorytm ma więcej danych i może lepiej dokonywać kategoryzacji.<br><br>
+
+            <h5>Ograniczenia</h5>
+
+            System zaciąga dane z Twittera poprzez tzw. „stream”. Twitter nie informuje podmiotów korzystających ze streama
+            o wprowadzanych tam technicznych zmianach. W związku z tym są okresy, w których system nie pobierał danych. Są
+            to:
+            <ul>
+                <li> lipiec-sierpień 2022 r.;
+                </li>
+                <li> październik 2022 r.
+                </li>
+            </ul>
+
+        </div>
     </div>
 
     <div class="card card-frame" style="margin-bottom: 16px">
         <div class="card-body">
-            <h3>FUNKCJONALNOŚCI COUNTERHATE </h3>
+            <h3>O projekcie</h3><br>
+            Powstanie i popularyzacja mediów społecznościowych stworzyły nowy, równoległy do tradycyjnych mediów ekosystem
+            obiegu informacji. Przez media społecznościowe dowolne, niezweryfikowane albo szkodliwe treści mogą trafiać do
+            setek tysięcy odbiorców. Tworzą się więc warunki, w których mowa nienawiści może szczególnie łatwo się
+            rozprzestrzeniać. A to potrafi realnie wpływać na zmiany postaw społecznych na wielką skalę. W Polsce masowe
+            kampanie dezinformacyjne doprowadzały w przeszłości choćby do zwiększenia niechęci do niektórych grup
+            mniejszościowych – uchodźców, Żydów, społeczności LGBTQ i ogólnie przedstawicieli innych kultur. <br><br>
 
+            W ramach projektu powstały dwa raporty analizujące nienawistne dyskursy w mediach społecznościowych: <br><br>
 
-            <h5>Zakładka „Szukaj” (w wersji okrojonej dostępna bez logowania, w wersji pełnej po zalogowaniu)</h5>
-            {{-- (w wersji okrojonej dostępna bez logowania, w wersji pełnej po zalogowaniu) <br> --}}
-
-            <b>Wyszukiwanie treści</b> <br>
-
-            Podstawową dla tej zakładki funkcjonalnością jest możliwość wyszukiwania interesujących nas treści. Wyszukiwać
-            można na podstawie:
             <ul>
-                <li> „Zawiera treść” – wypowiedzi zawierające konkretne słowa, wyrażenia, itp.</li>
-                <li> „Nie zawiera treść” – odwrotność tego powyżej</li>
-                <li> „Konto do wyszukiwania” – wypowiedzi publikowane przez użytkownika o konkretnej nazwie</li>
-                <li> „Od” i „do” – wypowiedzi publikowane w skonkretyzowanym okresie (można doprecyzować datę, ale również
-                    godzinę)</li>
-                <li> „Kategorie” – wypowiedzi zakwalifikowane jako obraźliwe w stosunku do jednej z wyróżnionych mniejszości
+                <li> <a href="https://geremek.pl/publikacja/granice_nienawisci/">Granice Nienawiści</a> analiza mowy
+                    nienawiści towarzyszącą dwóm kryzysom uchodźczym: związanym z inwazją
+                    Rosji na Ukrainę oraz z sytuacją na granicy polsko-białoruskiej.
                 </li>
-                <li> „Zawiera słowa kluczowe” – wypowiedzi zawierające wybrane słowo-klucz spośród wcześniej wyróżnionych
-                </li>
-                <li> „Nie zawiera słów kluczowych” - odwrotność tego powyżej </li>
-            </ul>
-
-            Różne parametry wyszukiwania można ze sobą mieszać, czyli na przykład można wyszukać wypowiedzi opublikowane
-            przez użytkownika @jan_nowak w okresie od 1 stycznia 2023 do 2 lutego 2023 roku zawierające słowo „banan” – i
-            tym podobne.<br><br>
-
-            Wyszukane treści można np. pobrać do Excela do dalszych analiz.<br><br>
-
-            Takie wyszukiwanie pozwala na:
-            <ul>
-                <li> stworzenie prostej bazy wypowiedzi, które są dla nas interesujące ze względu na dany parametr – co
-                    pozwala np.
-                    na badanie konkretnego wątku w interesującej nas mowie nienawiści</li>
-                <li> przeprowadzenie wstępnych obserwacji internetowej mowy nienawiści do dalszych działań (monitoringowych,
-                    edukacyjnych)</li>
-                <li> zebranie przykładów internetowej nienawiści na potrzeby np. warsztatów, zajęć ze studentami i tym
-                    podobnych
-                </li>
-                <li>obserwację powtarzających się wzorców, czy wręcz kopiowanych wielokrotnie w różnych kontekstach
-                    wypowiedzi
-                    (co
-                    jest cechą charakterystyczną m.in. trolli)
+                <li> <a href="https://geremek.pl/publikacja/przyjda_i_zabiora/">Przyjdą i zabiorą</a> analiza
+                    antyukraińskiej mowy nienawiści.
                 </li>
             </ul>
 
-            <h5>Linkowanie do Twittera</h5>
 
-            Przy każdej wypowiedzi istnieje możliwość skorzystania z przekierowania do Twittera i sprawdzenie, jak
-            prezentuje się ona w szerszym kontekście – czy jest odpowiedzią na coś, czy jest elementem dyskusji, itp. To
-            bardzo ważna możliwość w bardziej wnikliwej jakościowej analizie treści, a także jeśli szukamy wzajemnych
-            powiązań i dynamik rozprzestrzeniania się nienawistnych treści.<br><br>
+            Grantodawca: Komisja Europejska: Justice Programme (JUST); Rights, Equality and Citizenship Programme (REC)
+            <br><br>
 
-            np. w najnowszym raporcie pozwoliła na wyróżnienie taktyki „podczepiania” się trolli i siewców nienawiści pod
-            popularniejsze profile celem zwiększenia zasięgów <br><br>
-
-            Ponadto, system może każdorazowo pobrać dane dotyczące liczby reakcji na daną wypowiedź – ale sprawdzać to można
-            tylko pojedynczo. To daje możliwość sprawdzenia popularności konkretnych wpisów, ich viralowości.<br><br>
-
-
-
-            <h5>Wykresy i rankingi</h5>
-
-
-
-            Zakładka szukaj posiada jednak nie tylko opcję „Pokaż wpisy”, ale również „Pokaż statystyki”, która pozwala na
-            przyjrzenie się wyszukiwanym treściom także pod względem ilościowym.<br><br>
-
-            Są dostępne cztery rankingi porządkujące najpopularniejsze:
+            Autorzy systemu:
             <ul>
-                <li> słowa-klucze (w wersji pełnej deklinacji lub pojedynczych form fleksyjnych) </li>
-                <li> czas </li>
-                <li> kategorie </li>
-                <li> konta </li>
-            </ul>
+                <li><a href="https://geremek.pl/">Fundacja Geremka</a></li>
 
-            Dane z rankingów można pobierać do Excela do dalszych analiz.<br><br>
+                <li><a href="https://hfhr.pl/">Helsińska Fundacja Praw Człowieka</a></li>
 
-            Dodatkowo, system na podstawie wygenerowanych danych może tworzyć wykresy, które również można pobrać. Możliwa
-            jest regulacja liczby recordów pojawiających się na wykresie (np. 10, 20, 50 najpopularniejszych).<br><br>
-
-
-            <h5>Zakładka „Anotuj” (dostępna po zalogowaniu)</h5>
-
-            Pozwala użytkownikom na ocenę zebranych przez system treści pod kątem tego, czy należy uznać je za mowę
-            nienawiści, czy nie – a jeśli tak, to jakiej grupy dotyczy nienawiść. Jest to więc „cegiełka”, którą użytkownik
-            może dołożyć do rozwoju systemu J – dzięki temu algorytm ma więcej danych i może lepiej dokonywać
-            kategoryzacji.<br><br>
-
-            Dla użytkowników może być o tyle pomocna, że pozwala na:
-            <ul>
-                <li> poobcowanie z mową nienawiści, obserwację jej tropów i mechanizów, która może być punktem wyjścia do
-                    dalszego
-                    poszukiwania/badania</li>
-                <li> urefleksyjnienie tego, co to znaczy, że coś jest „mową nienawiści”
-                </li>
-                <li> obserwację własnych schematów i przyzwyczajeń myślowych związanych z tym, co uważamy za „mowę
-                    nienawiści”
-                </li>
+                <li>SecureLex Sp. z o.o.</li>
             </ul>
         </div>
     </div>
